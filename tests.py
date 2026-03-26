97b729e6ee61a65da08f8f5cfc5ded5848d49068
Wesley Marques <wesleymarquesdc@gmail.com>
2026-03-26 20:17:52 -0300

    Commit 2: Creating 10 unit tests

--- tests.py
+++ tests.py
@@ -25,6 +25,14 @@ def test_create_question_with_valid_points():
     question = Question(title='q1', points=100)
     assert question.points == 100
 
+def test_create_question_with_invalid_points():
+    with pytest.raises(Exception):
+        Question(title='q1', points=0)
+    with pytest.raises(Exception):
+        Question(title='q1', points=-1)
+    with pytest.raises(Exception):
+        Question(title='q1', points=101)
+
 def test_create_choice():
     question = Question(title='q1')
     
@@ -34,3 +42,86 @@ def test_create_choice():
     assert len(question.choices) == 1
     assert choice.text == 'a'
     assert not choice.is_correct
+
+def test_remove_choice():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+
+    question.remove_choice_by_id(question.choices[0].id)
+
+    assert len(question.choices) == 0
+
+def test_remove_choice_invalid():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.remove_choice_by_id(1)
+
+def test_remove_all_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+    question.add_choice('b')
+
+    question.remove_all_choices()
+
+    assert len(question.choices) == 0
+
+def test_set_correct_choices():
+    question = Question(title='q1')
+
+    question.add_choice('a')
+    question.add_choice('b')
+
+    questionA = question.choices[0]
+    questionB = question.choices[1]
+
+    question.set_correct_choices([questionA.id])
+
+    assert questionA.is_correct == True
+    assert questionB.is_correct == False
+
+def test_set_correct_choices_invalid():
+    question = Question(title='q1')
+
+    with pytest.raises(Exception):
+        question.set_correct_choices([1])
+
+def test_set_correct_choices_empty():
+    question = Question(title='q1')
+
+    question.set_correct_choices([])
+
+def test_correct_selected_choices_empty():
+    question = Question(title='q1')
+
+    question.add_choice('a', True)
+
+    correct_selected_choices = set(question.correct_selected_choices([]))
+
+    assert set() == correct_selected_choices
+
+def test_correct_selected_choices_correct():
+    question = Question(title='q1')
+
+    question.add_choice('a', True)
+    question.add_choice('b')
+
+    questionA = question.choices[0]
+
+    correct_selected_choices = set(question.correct_selected_choices([questionA.id]))
+
+    assert {questionA.id} == correct_selected_choices
+
+def test_correct_selected_incorrect():
+    question = Question(title='q1')
+
+    question.add_choice('a', True)
+    question.add_choice('b')
+
+    questionB = question.choices[1]
+
+    correct_selected_choices = set(question.correct_selected_choices([questionB.id]))
+
+    assert set() == correct_selected_choices
